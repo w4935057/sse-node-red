0adde749b13026feb538cd785b08351d35ffb789
e.corona <e.corona@a-key.it>
2017-04-10 16:07:01 +0200

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # sse-node-red
-Enhanced Server Sent Events node for NODE-RED
-Enhanced version of PointSource/node-red-contrib-sse (https://github.com/PointSource/node-red-contrib-sse),
+
+## Install
+
+To install run: 
+
+`npm install node-red-contrib-sse-en --save`
+
+Until the module is not registered on npm use the following to install:
+
+`npm install ekaralis/node-red-contrib-sse-en`
+
+## Description
+
+Enhanced Server Sent Events node for NODE-RED 
+
+This is an enhanced version of PointSource/node-red-contrib-sse (https://github.com/PointSource/node-red-contrib-sse), 
 accepting "room" parameter to be set via message object (inside msg.room)
---
+++ src/sse.js
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2016 PointSource, LLC.
+ * Code Enhanced by (c) 2017 
+ * Enrico Corona - eKaralis.it
+ * (https://github.com/arakno76)
+ * MIT Licensed
+ */
+var sse = require('simple-sse'),
+    http = require('http');
+
+/**
+ * The exported module for the Node-RED package to pick up 
+ * the sse node. It will register the sse node type.
+ * @param  {object} RED Required parameter for Node-RED
+ */
+module.exports = function(RED) {
+    /**
+     * @name CreateSSENode
+     * @description Will create the sse node and setup all the variable. 
+     * It will register.
+     * a http listener on the path and room inputed to the node. 
+	 * Room can even be specified in input message, inside msg.room
+     * @param {object} config Configuration passed in from Node-RED library
+     */
+    function CreateSSENode(config) {
+        // Do the initialization for Node-RED
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        // Initialize variables using the user inputs
+        var room = config.room;
+        var path = config.path + "/" + ':roomId';
+        var retry = config.retrySet !== undefined ? config.retry : undefined;
+        var accessAllowControlOrigin =
+            config.accessControlSet !== undefined ? config.accesscontrol : undefined;
+        var clientNums = 0;
+        sse.heartbeat = config.heartbeat;
+
+        // Set up the status Icon at the beginning
+        updateNode();
+
+        // The route for specific path and room
+        RED.httpNode.get(path , function(req, res) {
+            if (accessAllowControlOrigin) {
+              res.setHeader('Access-Control-Allow-Origin', accessAllowControlOrigin)
+            }
+            clientNums++;
+            // create client sse
+            var client = sse.add(req, res);
+
+            // set sse retry
+            if (retry && retry > 0) {
+                sse.setRetry(client, config.retry);
+            }
+
+            // add to the room
+			var localRoom = req.params.roomId ? req.params.roomId : room;
+            sse.join(client, localRoom);
+
+            // update the status Icon
+            updateNode();
+
+            req.connection.addListener("close", function() {
+                clientNums--;
+                updateNode();
+            });
+        });
+
+        // On input broadcast the massage to all clients
+        this.on('input', function(msg) {
+			var localRoom = msg.room? msg.room : room;
+            if (msg.event) {
+                sse.broadcast(localRoom, msg.event, msg.payload);
+            }
+            sse.broadcast(localRoom, msg.payload);
+        });
+
+        // When closing remove the route listener
+        this.on("close", function() {
+            RED.httpNode._router.stack.forEach(function(route, i, routes) {
+                if (route.route &&
+                    route.route.path === path &&
+                    route.route.methods['get']) {
+                    routes.splice(i, 1);
+                }
+            });
+        });
+
+        /**
+         * @name updateNode
+         * @description Will update the status Icon of the node based on 
+         * number of clients
+         */
+        function updateNode() {
+            if (clientNums === 0) { // No client connected
+                node.status({
+                    fill: "red",
+                    shape: "ring",
+                    text: "disconnected"
+                });
+            } else {
+                node.status({
+                    fill: "green",
+                    shape: "dot",
+                    text: clientNums + " client(s) connected"
+                });
+            }
+        }
+    }
+
+    RED.nodes.registerType("sse-en", CreateSSENode);
+}
